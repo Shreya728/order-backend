d2c17e98cef59a91610e7baea7a7e3bd765e2ea1
shreya pateriya <shreyapateriya728@gmail.com>
2025-03-15 22:46:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv  # Add this
-import os
 
-# Load environment variables first!
-load_dotenv()
-
-# Then get DATABASE_URL
-# In database.py
-DATABASE_URL = "postgresql://postgres@localhost:5432/orderdb1"  # No password
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
-Base = declarative_base()
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
